1e1f8457d2c15ad78472149358be5aa5f3682eed
kleindesign <danielkleindesign@gmail.com>
2024-12-03 13:25:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 }
 
 export default nextConfig;
